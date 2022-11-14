df5e004115cb490b4fa005b3633bf7c9038b0133
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-14 13:01:49 -0700

    Add challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -1,7 +1,38 @@
-const citiesToVisit = [`Chicago`, `Toronto`, `Athens`, `Tokyo`, `Tuscany`];
+var citiesToVisit = ["Chicago", "Toronto", "Athens", "Tokyo", "Tuscany"] 
+for(var i = 0) i < citiesToVisit.length; i++) {
+           if (names[i].startsWith("t"))}
+         }
+         console.log(names[i])} 
 
-let startsWithT = countries.filter(function (city) {
-  return country[0].toLowerCase() === 't';
-});
 
-console.log(startsWithT)
+
+
+
+
+
+function startsWithT(array){
+    for(i = 0; i < array.length; i++){
+    console.log(array[i].startswithT("T"))}
+}
+startsWithT(citiesToVisit)
+
+
+
+// let startsWithT = countries.filter(function (city) {
+//   return citiesToVisit[0].startsWithT() === 't';
+// });
+
+// console.log(startsWithT)
+// for 
+// });
+
+// console.log(startsWithT);
+
+// var fiends = ["Sara", "Taylor", "Sasha", "Mary", "Lex"] {
+//     for(var i = 0) i < friends.length; i++) {
+//       if (names[i].startsWith("s"))}
+//     }
+//     console.log(names[i])}
+
+
+// if (array[i].ctitiesToVisit.startsWith("t"))
