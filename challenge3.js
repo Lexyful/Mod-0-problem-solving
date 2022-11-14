de7b3e66677ac1796c4056d71eb2d90d45e1171d
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-13 20:49:49 -0700

    Add challenge3

---
+++ challenge3.js
@@ -0,0 +1,7 @@
+const citiesToVisit = [`Chicago`, `Toronto`, `Athens`, `Tokyo`, `Tuscany`];
+
+let startsWithT = countries.filter(function (city) {
+  return country[0].toLowerCase() === 't';
+});
+
+console.log(startsWithT)
